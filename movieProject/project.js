fd4123bdc10e1740033468c29f72027d10af28cc
unknown <abisovfarid14@gmail.com>
2024-03-10 18:58:52 +0400

    delete film

--- movieProject/project.js
+++ movieProject/project.js
@@ -2,7 +2,7 @@ const form=document.getElementById("film-form");
 const titleElement=document.querySelector("#title");
 const directorELement=document.querySelector("#director");
 const urlELement=document.querySelector("#url");
-
+const secondCardBody=document.querySelectorAll(".card-body")[1]
 //UI objectini baslatma
 const ui=new UI();
 
@@ -19,8 +19,9 @@ function eventListeners(){
       let films=storage.getFilmsFromStorage();
       ui.loadAllFilms(films);
 
-      
+
     });
+    secondCardBody.addEventListener("click",deletefilm);
 }
 
 function addFilm(e){
@@ -40,4 +41,10 @@ function addFilm(e){
     ui.clearInputs(titleElement,urlELement,directorELement);
      
     e.preventDefault()
+}
+
+function deletefilm(e){
+   if(e.target.id==="delete-film"){
+    ui.deleteFilmFromUI(e.target);
+   }
 }
--- movieProject/ui.js
+++ movieProject/ui.js
@@ -40,3 +40,7 @@ UI.prototype.loadAllFilms=function (films){
     `
   })
 }
+
+UI.prototype.deleteFilmFromUI=function(element){
+    element.parentElement.parentElement.remove();
+}
